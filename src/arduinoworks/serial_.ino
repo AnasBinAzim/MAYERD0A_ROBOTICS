0630fd9b387551b0d34d84d1e9beed00dd983c08
AnasBinAzim <anas.azim.71@gmail.com>
2024-11-28 03:34:21 +0600

    Delete src/arduinoworks/serial_.ino

--- src/arduinoworks/serial_.ino
+++
@@ -1,7 +0,0 @@
-void srl() {
-  // Non-blocking Serial check
-  while (Serial.available() > 0) { // Loop until all serial data is read
-    int_it = Serial.read();        // Read a byte from the serial buffer
-    obj = int_it - '0';            // Convert ASCII character to integer (assuming single digit)
-  }
-}
